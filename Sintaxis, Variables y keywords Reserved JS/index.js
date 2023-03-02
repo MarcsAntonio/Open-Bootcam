1e85970af5fea7b1fe86f7b2ca0b3a784af82328
Marcs <marcostrujillo15@gmail.com>
2023-03-01 22:07:04 -0400

    Ejercicio 2

---
+++ Sintaxis, Variables y keywords Reserved JS/index.js
@@ -0,0 +1,16 @@
+let nombre = "Marcos A";
+
+let edad = 25;
+
+let desarrollador = false;
+
+let nacimiento = new Date(97,9,26);
+
+let libro = {
+    titulo: "Padre Rico, Padre Pobre",
+    autores: "Robert T. Kiyosaki y Sharon L. Lechter",
+    fecha: 1997,
+    url: "https://www.unpa.edu.mx/~blopez/algunosLibros/Padre-Rico-Padre-Pobre.pdf",
+}
+
+const list = [nombre, edad, desarrollador, nacimiento, libro]
