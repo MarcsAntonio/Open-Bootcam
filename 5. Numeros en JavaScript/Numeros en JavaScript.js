a04071f9e14031257a64a7d214cf9c984b544bfa
Marcs <marcostrujillo15@gmail.com>
2023-04-08 00:54:33 -0400

    Ejercicio 5 de Numero de JavaScript

---
+++ 5. Numeros en JavaScript/Numeros en JavaScript.js
@@ -0,0 +1,17 @@
+let height = "1.71";
+let weight = "70.4";
+
+
+let c = parseInt(height*100);
+let M = parseFloat(height);
+
+let k = parseFloat(weight);
+
+let HM1 = Math.round(height);
+let WK2 = Math.round(weight);
+
+let Max1 = 1 + Number.MAX_VALUE;
+
+console.log(HM1);
+console.log(WK2);
+console.log(Max1);
