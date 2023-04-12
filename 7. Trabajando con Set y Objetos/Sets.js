a48fbf615897b931512920c86d3ceca3fe67056b
Marcs <marcostrujillo15@gmail.com>
2023-04-12 19:19:54 -0400

    Ejercicio 7.1 sets

---
+++ 7. Trabajando con Set y Objetos/Sets.js
@@ -0,0 +1,8 @@
+const nombres = new Set([`Marcos`,`Alfredo`,`Hector`,`Raul`])
+console.log(nombres);
+
+nombres.add("Marcos")
+console.log(nombres);
+
+nombres.add("Javascript")
+console.log(nombres);
